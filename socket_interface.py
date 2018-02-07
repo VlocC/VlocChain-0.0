0cc222b047c3c662eae930f306bf61bd9e0b24db
Owen J Sullivan <multiojuice@gmail.com>
2018-02-07 00:18:52 -0500

    Completed the transaction function using sockets, must be implemented into app.py

--- socket_interface.py
+++ socket_interface.py
@@ -19,7 +19,7 @@ def main():
     response = input("Please enter 'new' if you are a new user")
     username = ""
     #If they need to create a new user
-    if(response = "new" || response = "New"):
+    if(response == "new" || response == "New"):
         #Send keyword "new" and recieve username back
         s.send(encode("new"))
         username = s.recv(1024)
@@ -41,9 +41,54 @@ def main():
             if(response = True): break
 
     # At this point, the user should either have a new username or be verified
+    while(True):
+        direction = input("Input your desired actions (type one of the following):\n
+            transaction, view, exit")
+        if(direction == "exit"):
+            s.send(encode("exit"))
+            break
+        else if(direction == "view"):
+            view(username)
+        else if(direction == "transaction"):
+            transaction(username)
+
+    #After all actions are completed
+    s.shutdown(socket.SHUT_WR)
+    s.close()
 
 
+
+
+def view(username):
+    pass
+
+
+"""
+desc- sends files to another address
+param- username of the sender
+"""
+def transaction(username):
+    #get and send the recievers name to verify
+    while(True):
+        reciever = input("Enter the recievers address")
+        s.send(encode(reciever))
+        #check if it is verified
+        response = s.recv(1024)
+        if(response == True): break;
+    
+    #Both users verified, chose the file to send
+    filename = input("input a file to send")
+    filename = open(filename,'rb')
+    #take info from file
+    info = filename.read(1024)
+    while(info): #while therer is info to send
+        print("Sending data..")
+        #send it and read more
+        s.send(info)
+        info = filename.read(1024)
+
+    print("Transaction Completed")
+    filename.close()
+    
         
-
-
 
